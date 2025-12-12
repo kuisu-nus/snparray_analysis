64e1c2da92a876bda4769aae10ea967bc7c3259b
sukui <kuisu_dgut@163.com>
2025-12-12 17:09:46 +0800

    action of coordinates from merged VCF fil

--- scripts/vcf_process/3.merge_vcfs.sh
+++ scripts/vcf_process/3.merge_vcfs.sh
@@ -11,3 +11,6 @@ tabix -p vcf myfile.vcf.gz
 
 # merge vcf.gz files by same chromosome
 bcftools merge chr1_part1.vcf.gz chr1_part2.vcf.gz -Oz -o chr1_merged.vcf.gz
+
+# 从vcf.gz中提取坐标
+bcftools query -f '%CHROM\t%POS0\t%POS\n' Filter.ALL.genome.vcf.gz > union_snp_pos.bed
